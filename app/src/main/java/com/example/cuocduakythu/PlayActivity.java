45437b81db2e532676d5c86324dd02544d192440
HieuNghia0000 <fakenamemc1@gmail.com>
2024-05-20 00:57:58 +0700

    money feature, can bet multiple actor

--- app/src/main/java/com/example/cuocduakythu/PlayActivity.java
+++ app/src/main/java/com/example/cuocduakythu/PlayActivity.java
@@ -5,6 +5,7 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +21,9 @@ public class PlayActivity extends AppCompatActivity {
     Button btnStart;
     CheckBox checkBox, checkBox2, checkBox3;
     SeekBar seekBar, seekBar2, seekBar3;
+    EditText edtTienCuoc, edtTienCuoc2, edtTienCuoc3;
+
+    int tienCuoc = 100;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,6 +31,7 @@ public class PlayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play);
 
         mapping();
+        tvTienCuoc.setText(String.valueOf(tienCuoc));
 
         CountDownTimer countDownTimer = new CountDownTimer(60000, 300) {
             @Override
@@ -44,14 +49,29 @@ public class PlayActivity extends AppCompatActivity {
                 if (seekBar.getProgress() >= seekBar.getMax()) {
                     this.cancel();
                     Toast.makeText(PlayActivity.this, "Thú 1 thắng", Toast.LENGTH_SHORT).show();
+
+                    if (checkBox.isChecked()) {
+                        tienCuoc += Integer.parseInt(edtTienCuoc.getText().toString()) * 2;
+                        tvTienCuoc.setText(String.valueOf(tienCuoc));
+                    }
                 }
                 if (seekBar2.getProgress() >= seekBar2.getMax()) {
                     this.cancel();
                     Toast.makeText(PlayActivity.this, "Thú 2 thắng", Toast.LENGTH_SHORT).show();
+
+                    if (checkBox2.isChecked()) {
+                        tienCuoc += Integer.parseInt(edtTienCuoc2.getText().toString()) * 2;
+                        tvTienCuoc.setText(String.valueOf(tienCuoc));
+                    }
                 }
                 if (seekBar3.getProgress() >= seekBar3.getMax()) {
                     this.cancel();
                     Toast.makeText(PlayActivity.this, "Thú 3 thắng", Toast.LENGTH_SHORT).show();
+
+                    if (checkBox3.isChecked()) {
+                        tienCuoc += Integer.parseInt(edtTienCuoc3.getText().toString()) * 2;
+                        tvTienCuoc.setText(String.valueOf(tienCuoc));
+                    }
                 }
             }
 
@@ -64,11 +84,40 @@ public class PlayActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                seekBar.setProgress(0);
-                seekBar2.setProgress(0);
-                seekBar3.setProgress(0);
+                int totalTienCuoc = 0;
 
-                countDownTimer.start();
+                if (checkBox.isChecked() || checkBox2.isChecked() || checkBox3.isChecked()) {
+                    seekBar.setProgress(0);
+                    seekBar2.setProgress(0);
+                    seekBar3.setProgress(0);
+
+                    if (checkBox.isChecked()) {
+                        int tienCuoc1 = Integer.parseInt(edtTienCuoc.getText().toString());
+                        totalTienCuoc += tienCuoc1;
+                    }
+                    if (checkBox2.isChecked()) {
+                        int tienCuoc2 = Integer.parseInt(edtTienCuoc2.getText().toString());
+                        totalTienCuoc += tienCuoc2;
+                    }
+                    if (checkBox3.isChecked()) {
+                        int tienCuoc3 = Integer.parseInt(edtTienCuoc3.getText().toString());
+                        totalTienCuoc += tienCuoc3;
+                    }
+
+                    if (tienCuoc >= totalTienCuoc) {
+                        countDownTimer.start();
+                        tienCuoc -= totalTienCuoc;
+                        tvTienCuoc.setText(String.valueOf(tienCuoc));
+                    }
+                    else {
+                        Toast.makeText(PlayActivity.this, "Không đủ tiền đặt cược", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+                else {
+                    Toast.makeText(PlayActivity.this, "Vui lòng đặt cược!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -82,5 +131,8 @@ public class PlayActivity extends AppCompatActivity {
         seekBar = findViewById(R.id.seekBar);
         seekBar2 = findViewById(R.id.seekBar2);
         seekBar3 = findViewById(R.id.seekBar3);
+        edtTienCuoc = findViewById(R.id.editTextNumber);
+        edtTienCuoc2 = findViewById(R.id.editTextNumber2);
+        edtTienCuoc3 = findViewById(R.id.editTextNumber3);
     }
 }
