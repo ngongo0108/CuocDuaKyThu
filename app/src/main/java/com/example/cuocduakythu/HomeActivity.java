56e705fd84de12a8cac5b7fe5767f8fa6957a67e
ngongo <thuhuong562002@gmail.com>
2024-05-19 16:15:23 +0700

    fix

---
+++ app/src/main/java/com/example/cuocduakythu/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.cuocduakythu;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+//    Button btnPlay, btnUse, btnExits;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_play);
+//
+//        btnPlay = findViewById(R.id.btnPlay);
+//        btnUse = findViewById(R.id.btnUse);
+//        btnExits = findViewById(R.id.btnExits);
+//
+//
+//    }
+//}
--- app/src/main/java/com/example/cuocduakythu/MainActivity.java
+++ app/src/main/java/com/example/cuocduakythu/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.cuocduakythu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,14 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         anhXa();
-
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Vui lòng chờ giây lát", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     private void anhXa(){
         btnPlay = findViewById(R.id.btnPlay);
