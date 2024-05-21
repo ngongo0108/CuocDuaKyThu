be127268a88b74381a43baf63b7215c7c49acc68
Dan <brunotran1602@gmail.com>
2024-05-21 12:50:08 +0700

    Login-logout fix

--- app/src/main/java/com/example/cuocduakythu/HomeActivity.java
+++ app/src/main/java/com/example/cuocduakythu/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.cuocduakythu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
@@ -9,5 +12,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btnExits = findViewById(R.id.btnExits);
+        btnExits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
